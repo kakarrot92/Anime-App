bac145db046f3c89325a41f693db94ba63e8abb4
kakarrot92 <swiftblade1992@gmail.com>
2022-06-26 13:21:55 +0200

    instaling parcel

---
+++ dist/index.a77869d3.js
@@ -0,0 +1,11 @@
+const base_url = "https://api.jikan.moe/v4/anime";
+function pageLoaded() {
+    fetch(base_url).then((res)=>res.json()).then((data)=>console.log(data.data[2].title)).catch((err)=>console.warn(err.message));
+}
+window.addEventListener("load", pageLoaded); // fetch("https://api.jikan.moe/v4/anime").then((data)=> {
+ //     return data.json();
+ // }).then((completeData) => {
+ //     console.log(completeData.data[0].title);
+ // })
+
+//# sourceMappingURL=index.a77869d3.js.map
--- src/js/fetchApi.js
+++ src/js/fetchApi.js
@@ -1,16 +1,16 @@
-// const base_url = "https://api.jikan.moe/v4/anime";
+const base_url = "https://api.jikan.moe/v4/anime";
 
-// function pageLoaded() {
-//     fetch(base_url)
-//     .then(res => res.json())
-//     .then(data => console.log(data[2].title))
-//     .catch(err => console.warn(err.message));
-// }
+function pageLoaded() {
+    fetch(base_url)
+    .then(res => res.json())
+    .then(data => console.log(data.data[2].title))
+    .catch(err => console.warn(err.message));
+}
 
-// window.addEventListener("load",pageLoaded);
+window.addEventListener("load",pageLoaded);
 
-fetch("https://api.jikan.moe/v4/anime").then((data)=> {
-    return data.json();
-}).then((completeData) => {
-    console.log(completeData.data[0].title);
-})
+// fetch("https://api.jikan.moe/v4/anime").then((data)=> {
+//     return data.json();
+// }).then((completeData) => {
+//     console.log(completeData.data[0].title);
+// })
