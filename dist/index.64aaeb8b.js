4e1dc71fb68c4c77e643716b2a1ceade6fc3ff9d
kakarrot92 <swiftblade1992@gmail.com>
2022-07-08 22:06:17 +0200

    add the search functionality

--- dist/index.64aaeb8b.js
+++ dist/index.64aaeb8b.js
@@ -532,15 +532,70 @@ function hmrAcceptRun(bundle, id) {
 }
 
 },{}],"1eTVv":[function(require,module,exports) {
-const base_url = "https://api.jikan.moe/v4/anime";
-function pageLoaded() {
-    fetch(base_url).then((res)=>res.json()).then((data)=>console.log(data.data[2].title)).catch((err)=>console.warn(err.message));
+const form = document.querySelector(".search");
+const main = document.querySelector(".main");
+const moviesContainer = document.querySelector(".movies-container");
+// let animes = [];
+// const options = {
+// 	method: 'GET',
+// 	headers: {
+// 		'X-RapidAPI-Key': 'ba8638c110msh726453a99dd1496p13176djsnf6e27961969a',
+// 		'X-RapidAPI-Host': 'anime-db.p.rapidapi.com'
+// 	}
+// };
+// const showAnime = async function () {
+//     try {
+//         const res = await fetch('https://anime-db.p.rapidapi.com/anime?page=1&size=30', options);
+//         const data = await res.json();
+//         let anime = data.data;
+//         //Rendering Anime
+//      anime.map((animes)=> {
+//         const {title,image,episodes} = animes;
+//         const animeCard = document.createElement('div');
+//         animeCard.classList.add('box');
+//         animeCard.innerHTML = `
+//             <div class="box-img">
+//                 <img src = "${image}">
+//             </div>
+//             <h3>${title}</h3>
+//             <span>${episodes}</span>
+//             `
+//         moviesContainer.appendChild(animeCard);
+//     });
+//     return animes;
+//     }
+//     catch (err) {
+//         alert(err);
+//     }
+// };
+// showAnime();
+let animes = [];
+function searchShow(query) {
+    const url = `https://api.jikan.moe/v4/anime?q=${query}`;
+    fetch(url).then((response)=>response.json()).then((jsonData)=>{
+        let anime = jsonData.data;
+        console.log(anime);
+        anime.map((animes1)=>{
+            const { title , images , episodes  } = animes1;
+            const animeCard = document.createElement("div");
+            animeCard.classList.add("box");
+            animeCard.innerHTML = `
+            <div class="box-img">              
+               <img src = "${images.jpg.image_url}">             
+            </div>             
+            <h3>${title}</h3>             
+            <span>${episodes}</span>  
+             `;
+            moviesContainer.appendChild(animeCard);
+        });
+    });
 }
-window.addEventListener("load", pageLoaded); // fetch("https://api.jikan.moe/v4/anime").then((data)=> {
- //     return data.json();
- // }).then((completeData) => {
- //     console.log(completeData.data[0].title);
- // })
+window.onload = ()=>{
+    const search = document.querySelector(".search-field");
+    search.onkeyup = (event)=>{
+        searchShow(search.value);
+    };
+};
 
 },{}]},["jgd9a","1eTVv"], "1eTVv", "parcelRequirecec7")
 
--- src/js/fetchApi.js
+++ src/js/fetchApi.js
@@ -1,16 +1,83 @@
-const base_url = "https://api.jikan.moe/v4/anime";
+const form = document.querySelector('.search');
 
-function pageLoaded() {
-    fetch(base_url)
-    .then(res => res.json())
-    .then(data => console.log(data.data[2].title))
-    .catch(err => console.warn(err.message));
+const main = document.querySelector('.main');
+const moviesContainer = document.querySelector('.movies-container');
+
+
+
+// let animes = [];
+
+// const options = {
+// 	method: 'GET',
+// 	headers: {
+// 		'X-RapidAPI-Key': 'ba8638c110msh726453a99dd1496p13176djsnf6e27961969a',
+// 		'X-RapidAPI-Host': 'anime-db.p.rapidapi.com'
+// 	}
+// };
+// const showAnime = async function () {
+//     try {
+//         const res = await fetch('https://anime-db.p.rapidapi.com/anime?page=1&size=30', options);
+//         const data = await res.json();
+//         let anime = data.data;
+//         //Rendering Anime
+       
+//      anime.map((animes)=> {
+//         const {title,image,episodes} = animes;
+//         const animeCard = document.createElement('div');
+//         animeCard.classList.add('box');
+//         animeCard.innerHTML = `
+//             <div class="box-img">
+//                 <img src = "${image}">
+//             </div>
+//             <h3>${title}</h3>
+//             <span>${episodes}</span>
+//             `
+//         moviesContainer.appendChild(animeCard);
+       
+//     });
+//     return animes;
+//     }
+     
+    
+//     catch (err) {
+//         alert(err);
+//     }
+// };
+// showAnime();
+let animes = [];
+function searchShow(query) {
+    
+    const url = `https://api.jikan.moe/v4/anime?q=${query}`;
+    fetch(url)
+    .then(response => response.json())
+    .then((jsonData) => {
+        let anime = jsonData.data;
+        console.log(anime);
+         anime.map((animes)=> {
+            const {title,images,episodes} = animes;
+            const animeCard = document.createElement('div');
+            animeCard.classList.add('box');
+            animeCard.innerHTML = `
+            <div class="box-img">              
+               <img src = "${images.jpg.image_url}">             
+            </div>             
+            <h3>${title}</h3>             
+            <span>${episodes}</span>  
+             `
+        moviesContainer.appendChild(animeCard);
+            
+         });
+        
+    })
 }
 
-window.addEventListener("load",pageLoaded);
+window.onload = () => {
+    const search = document.querySelector('.search-field');
+    search.onkeyup = (event) => {
+        searchShow(search.value);
+    }
+}
 
-// fetch("https://api.jikan.moe/v4/anime").then((data)=> {
-//     return data.json();
-// }).then((completeData) => {
-//     console.log(completeData.data[0].title);
-// })
+
+
+
